c2f4f276afad3f07b5d111b81cf66d87965a4710
Thinh Tran <thinhtran@outlook.com.au>
2019-08-24 21:25:20 +0800

    add toggle buttons visibility

--- ChatUIXForms/Views/Partials/ChatInputBarView.xaml.cs
+++ ChatUIXForms/Views/Partials/ChatInputBarView.xaml.cs
@@ -7,9 +7,14 @@ namespace ChatUIXForms.Views.Partials
 {
     public partial class ChatInputBarView : ContentView
     {
+
+
         public ChatInputBarView()
         {
             InitializeComponent();
+
+              isButtonsVisible = true;
+            expandButton.IsVisible = false;
 
             if (Device.RuntimePlatform == Device.iOS)
             {
@@ -22,10 +27,54 @@ namespace ChatUIXForms.Views.Partials
             chatTextInput.Focus();
 
             UnFocusEntry();
+            ToggleButtons();
+        }
+
+
+        private void ExpandButton_Clicked(object sender, EventArgs e)
+        {
+            expandButton.IsVisible = false;
+            micButton.IsVisible = true;
+            speakerButton.IsVisible = true;
+        }
+
+        private void ChatTextInputTapped(object sender, EventArgs e)
+        {
+
         }
 
         public void UnFocusEntry(){
             chatTextInput?.Unfocus();
+        }
+
+     
+
+        private bool isButtonsVisible;
+        public void ToggleButtons()
+        {
+            isButtonsVisible = !isButtonsVisible;
+          
+
+           
+            if (isButtonsVisible)//chatTextInput.IsFocused)
+            {
+                //Fading buttons from right to left order
+                micButton.FadeTo(0, 1500, Easing.SpringIn);
+                micButton.IsVisible = false;
+
+                speakerButton.FadeTo(0, 1500, Easing.SpringIn);
+                speakerButton.IsVisible = false;
+
+                expandButton.IsVisible = true;
+            }
+            else
+            {
+                micButton.Opacity = 1;
+               // micButton.IsVisible = isButtonsVisible;
+                speakerButton.Opacity = 1;
+                //speakerButton.IsVisible = isButtonsVisible;
+
+            }
         }
 
     }
